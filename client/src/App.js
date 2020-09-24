1dd1babc8e3368210c5bc91cefc8a1d1b06daaa1
Nathan Szurek <nathan.szurek@yahoo.com>
2020-09-24 17:37:53 -0500

    Created basic bootstrap navbar

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import { BrowserRouter as Router, Route } from "react-router-dom";
-import NavBar from "../components/Navbar";
-import Home from "../pages/Home";
-import Contact from "../pages/Contact";
-import Portfolio from "../pages/Portfolio";
+import NavBar from "./components/Navbar";
+import Home from "./pages/Home";
+import Contact from "./pages/Contact";
+import Portfolio from "./pages/Portfolio";
 
 
 
