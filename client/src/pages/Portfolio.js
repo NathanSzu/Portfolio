d080663e5965ad6e3bd7ddf3fc5c19340f917525
Nathan Szurek <nathan.szurek@yahoo.com>
2020-09-25 08:06:20 -0500

    Creaed basiccontact and portfolio pages

--- client/src/pages/Portfolio.js
+++ client/src/pages/Portfolio.js
@@ -1,9 +1,22 @@
 import React from 'react'
+import ProfileCard from '../components/ProfileCard'
 
 export default function Portfolio() {
     return (
         <div>
-            
+            <div className="d-flex justify-content-center">
+                <div className="jumbotron col-md-8 jumbotron-fluid">
+                    <div className="row">
+                        <div className="col-md-4">
+                            <ProfileCard />
+                        </div>
+                        <div className="col-md-8">
+                            <h1 className="display-4">Fluid jumbotron</h1>
+                            <p className="lead">This is a modified jumbotron that occupies the entire horizontal space of its parent.</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
