4d84f5b2aed818645325e4a7ab7df8db95cf58ce
Nathan Szurek <nathan.szurek@yahoo.com>
2020-10-19 17:07:47 -0500

    Added resume and LinkedIn links

--- client/src/components/ProfileCard.js
+++ client/src/components/ProfileCard.js
@@ -6,8 +6,16 @@ export default function ProfileCard() {
     return (
         <div className="card" /*style="width: 18rem;"*/>
             <img className="card-img-top" src={ProfilePic} alt="Nathan Szurek" />
-            <div className="card-body">
+            <div className="card-body pb-0">
                 <p className="card-text">Full-stack Developer with a MERN stack development skill set and a passion for creating and learning.</p>
+            </div>
+            <div className="row center-wrapper">
+                <div className="col-xs-6 p-3">
+                    <a className="professional-link" href="https://drive.google.com/file/d/1xM-mLSowozpNPUQYFYXsG3uyMfo1xO6O/view?usp=sharing" target="_blank">Resume</a>
+                </div>
+                <div className="col-xs-6 p-3">
+                    <a className="professional-link" href="https://www.linkedin.com/in/nathanszurek" target="_blank">LinkedIn</a>
+                </div>
             </div>
         </div>
     )
